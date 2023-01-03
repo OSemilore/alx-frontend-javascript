bd085b7e56454b5573d154060d6dcab3d25ba4b1
OSemilore <oluwatosinigbayilola@gmail.com>
2023-01-03 13:29:48 -0500

    data manipulation

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(students) {
+    // check arg is an array before using map
+    if (Array.isArray(students)) {
+      return students.map((items) => items.id);
+    }
+    return [];
+  }
+  
